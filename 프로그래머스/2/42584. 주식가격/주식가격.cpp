61c3abf39f957fbdaf651223797dd46e28a16b04
이하택 <139848355+LeeHataeg@users.noreply.github.com>
2025-07-03 13:15:24 +0900

    [level 2] Title: 주식가격, Time: 26.12 ms, Memory: 26.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -1,21 +1,26 @@
-#include <bits/stdc++.h>
+#include <string>
+#include <vector>
+#include <stack>
 
 using namespace std;
 
 vector<int> solution(vector<int> prices) {
-    vector<int> answer;
+    vector<int> answer(prices.size());
     
-    ios::sync_with_stdio(false);
-    cin.tie(0);
+    stack<int> s;
     
-    for(int i = 0; i < prices.size(); i++){
-        answer.push_back(0);
-        for(int j = i + 1; j < prices.size(); j++){
-            answer[i]++;
-            
-            if(prices[i] > prices[j])
-                break;
+    int size = prices.size();
+    
+    for(int i = 0; i < size; i++){
+        while(!s.empty() && prices[s.top()] > prices[i]){
+            answer[s.top()] = i - s.top();
+            s.pop();
         }
+        s.push(i);
+    }
+    while(!s.empty()){
+        answer[s.top()] = size - s.top() -1;
+        s.pop();
     }
     
     return answer;
